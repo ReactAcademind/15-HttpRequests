0e17d9ac146dc303e01ea007b9dfc980934db6ed
Saul Pereira <saul_pereira@hotmail.com>
2024-12-13 08:01:18 -0600

    237. Transforming Fetched Data
    237. Transforming Fetched Data

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 
 import Places from './Places.jsx';
 import Error from './Error.jsx';
+import { sortPlacesByDistance } from '../loc.js';
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
@@ -20,12 +21,20 @@ export default function AvailablePlaces({ onSelectPlace }) {
           throw new Error('Failed to fetch places');
         }
 
-        setAvailablePlaces(resData.places);
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPLaces = sortPlacesByDistance(
+            resData.places, 
+            position.coords.latitude,
+            position.coords.longitude
+          );
+          setAvailablePlaces(sortedPLaces);
+          setIsFetching(false);
+        });
+
       } catch (error) {
           setError(error);
+          setIsFetching(false);
       }
-      
-      setIsFetching(false);
     }
 
     fetchPlaces();
